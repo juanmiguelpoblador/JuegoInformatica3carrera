62177df8c005a3a3f989eca13499b59bb8cd0a60
JUAN MIGUEL POBLADOR LOPEZ <juanmiguel.poblador@alumnos.upm.es>
2026-05-13 11:08:11 +0200

    tablero.h actualizado

--- ARCHON/Archon_Juego/Archon_Juego/tablero.h
+++ ARCHON/Archon_Juego/Archon_Juego/tablero.h
@@ -2,31 +2,33 @@
 #include <array>
 #include <vector>
 #include <memory>
-#include "Pieza.h" 
+#include <SFML/Graphics.hpp>
+#include "Pieza.h"
 
-enum class ResultadoVictoria { GanaAzul, GanaRojo, Ninguno }; //Decidimos el estado de la partida
+extern sf::RenderWindow* gVentana;
 
-struct Casilla { //Que hay en cada cuadro de la casilla 
-    bool esPuntoPoder = false; //true si es uno de los 5 puntos de poder, se aplican los bonificadores de curación y protección a la pieza encima
-    Pieza* pieza = nullptr; 
-}; //Usamos std::shared_ptr para no utilizar new al crear una pieza y tener que borrarla despues con delete
+enum class ResultadoVictoria { GanaAzul, GanaRojo, Ninguno };
+
+struct Casilla {
+    bool esPuntoPoder = false;
+    Pieza* pieza = nullptr;
+};
 
 class Tablero {
-public: //CHEKEAR CAMBIOS (VISUAL)
-    static constexpr int TAM = 9; //Tamaño de tablero
+public:
+    static constexpr int TAM = 9;
 
-    Tablero(); //constructor
+    Tablero();
     virtual ~Tablero();
     void inicializa();
     void dibuja() const;
     void mueve(double dt);
     void tecla(unsigned char key);
-    // Lógica de juego
-    bool moverPieza(int xOrig, int yOrig, int xDest, int yDest); //movimiento en el tablero. Recibe COORDENADAS de origen y de destino
-    void finalizarTurno(); //Limpiezatras movimiento. Cambia turno
-    ResultadoVictoria comprobarVictoria() const; //Comprueba estado
 
-    // Hechizos (Cerebro del juego)
+    bool moverPieza(int xOrig, int yOrig, int xDest, int yDest);
+    void finalizarTurno();
+    ResultadoVictoria comprobarVictoria() const;
+
     bool ejecutarHechizo(int idHechizo, int x, int y, int x2 = -1, int y2 = -1);
     void resolverCombate(Pieza* perdedor);
     Pieza* getAtacante() const;
@@ -34,13 +36,13 @@ public: //CHEKEAR CAMBIOS (VISUAL)
     bool hayCombatePendiente() const;
 
 private:
-    Casilla matriz[TAM][TAM];//9X9
-    Equipo turnoActual; //almacena  a quien le toca mover 
-    int contadorTurnos; //Cuenta los turnos para el ciclo de oscilacion (color de las casillas)
+    Casilla matriz[TAM][TAM];
+    Equipo turnoActual;
+    int contadorTurnos;
 
-    // Métodos auxiliares
-    bool esMovimientoLegal(Pieza* p, int xO, int yO, int xD, int yD) const; //comprueba si pieza puede llegar a su destino segun rango y tipo (Tierra,Vuelo,Tp)
-    void aplicarCuracion(); //Recorre tablero buscando piezas en punto de poder para aplicar bonificador
+    bool esMovimientoLegal(Pieza* p, int xO, int yO, int xD, int yD) const;
+    void aplicarCuracion();
+
     int atacanteX = -1, atacanteY = -1;
     int defensorX = -1, defensorY = -1;
 };
