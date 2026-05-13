9b90aa532096089c31c1f3f174a121f53f3bcf16
Carlos-Martinez-Marcos <carlos.martinez31@alumnos.upm.es>
2026-05-13 10:54:23 +0200

    class caballero

---
+++ ARCHON/Archon_Juego/Archon_Juego/caballero.cpp
@@ -0,0 +1,12 @@
+#include "caballero.h"
+#include <iostream> 
+
+
+// vida, fuerza, velocidadAtaque, tiempoRecarga, radioMovimiento, bando
+caballero::caballero(int bando)
+    : terrestre(150, 30, 5, 3, 3, bando) {
+}
+
+void caballero::atacarEnArena() {
+    
+}
---
+++ ARCHON/Archon_Juego/Archon_Juego/caballero.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "terrestre.h"
+
+class caballero : public terrestre {
+public:
+    caballero(int bando);
+    void atacarEnArena() override;
+    std::string getsimbolo() const override { return "caballero"; }
+};
